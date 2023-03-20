05f87db8a8566f9e0ae6add751192ce70acf8e68
Paa-Joo <david.afloet@gmail.com>
2023-03-20 02:14:50 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - all codes here
+ *
+ * Return: 0 if true
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
