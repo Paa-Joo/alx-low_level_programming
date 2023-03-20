7e549357747a8c108f49aa2129c93692f4be24fd
Paa-Joo <david.afloet@gmail.com>
2023-03-19 20:10:51 -0700

    Numbers again

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - all codes here
+ *
+ * Return: 0 if true
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
