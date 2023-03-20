e4feaf875ff8da02f5abe5ef84f5a911396a1ce4
Paa-Joo <david.afloet@gmail.com>
2023-03-19 19:57:24 -0700

    Excluding some alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - all codes here
+ *
+ * Return: 0 if true
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
