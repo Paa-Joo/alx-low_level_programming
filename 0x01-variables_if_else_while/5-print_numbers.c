8dc59af8c635d5140a43866d9ef2067bf4b96f4c
Paa-Joo <david.afloet@gmail.com>
2023-03-19 20:04:00 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - all codes here
+ *
+ * Return: 0 if true
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
