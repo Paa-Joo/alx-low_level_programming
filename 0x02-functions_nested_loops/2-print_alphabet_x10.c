57501ed009b93daaee9099ba46c1921d53358415
Paa-Joo <david.afloet@gmail.com>
2023-03-22 08:28:17 -0700

    10x printing

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printing 10x
+ *
+ * Return: 0 if true
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
